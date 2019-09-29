53b8d5bcd9e423a1458ce7d4c54ed141f585a8d1
Tom C <tomc@3gmagic.com>
2019-09-29 14:47:32 +0000

    new code

---
+++ play.py
@@ -0,0 +1,26 @@
+print "PLay to higher lower get 5 in a row to win!!"
+import random
+count  = 1
+n1 = random.randint(1, 10)
+while count < 6:
+    print  ("guesses %i" % count)
+    print n1
+    n2 = random.randint(1, 10)
+    while n1 == n2:
+        n2 = random.randint(1, 10)
+    # print n2
+    guess = raw_input("guess h/l? ")
+    if guess == "h":
+        if n2 > n1:
+            print "correct"
+        else:
+            print "wrong"
+            count = 0
+    else:
+        if n1 > n2:
+            print "correct"
+        else:
+            print "wrong"
+            count = 0
+    count = count + 1
+print "You win !!!"
